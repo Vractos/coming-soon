6cd72e6ade7037134044bc01a9ac452ba9c4227f
Vinicius <pauloviniciusrp9@gmail.com>
2022-05-27 12:09:07 -0300

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,27 @@
+import type { GatsbyConfig } from "gatsby";
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Coming Soon`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": process.env.GOOGLE_ANALYTICS_TRACKING_ID
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
+
+export default config;
