7d4983ae8cfa4a0a58d51aafe75b724bb34891b7
Vinicius <pauloviniciusrp9@gmail.com>
2022-06-01 17:49:28 -0300

    Adding support to Create Pages Programmatically

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import { graphql, PageProps, StaticQueryDocument } from 'gatsby'
+import { graphql, PageProps, Link } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from '../components/layout'
+import Layout from '../../components/layout'
 
 const BlogPage = ({ data }: PageProps<Queries.BlogPageQuery>) => {
   return (
     <Layout pageTitle='My Blog Posts'>
       {
-        data.allMdx.nodes.map((node) => (
+        data.allMdx.nodes.map(node => (
           <article key={node.id}>
-            <h2>{node.frontmatter?.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter?.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter?.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
           </article>
         ))
       }
@@ -30,7 +31,7 @@ export const query = graphql`
           title
         }
         id
-        body
+        slug
       }
     }
   }
