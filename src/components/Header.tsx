15c74516e93963eeee5ebd88d33d052fbf240a27
Vinicius <pauloviniciusrp9@gmail.com>
2022-06-21 21:39:26 -0300

    FIX window

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,22 +3,29 @@ import { AtratiLogoComplete, AtratiLogoMobile } from '../images/svg/AtratiLogo'
 
 
 const Header = () => {
-  const [width, setWidth] = useState(window.innerWidth);
+  let width;
+  if (typeof window !== `undefined`) {
+    width = window.innerWidth
+  }
+
+  const [widthDimension, setWidth] = useState(width);
   const breakpoint = 768;
 
   useEffect(() => {
-    const handleResizeWindow = () => setWidth(window.innerWidth);
+    const handleResizeWindow = () => setWidth(width);
      // subscribe to window resize event "onComponentDidMount"
-     window.addEventListener("resize", handleResizeWindow);
-     return () => {
-       // unsubscribe "onComponentDestroy"
-       window.removeEventListener("resize", handleResizeWindow);
-     };
+     if (typeof window !== `undefined`) {
+       window.addEventListener("resize", handleResizeWindow);
+       return () => {
+         // unsubscribe "onComponentDestroy"
+         window.removeEventListener("resize", handleResizeWindow);
+       };
+    }
    }, []);
 
   return (
     <header className='flex bg-atrati-gray md:bg-white h-20 md:h-24 w-full px-7 md:px-28 items-center'>
-      {width < breakpoint ? (<AtratiLogoMobile />) : (<AtratiLogoComplete />)}
+      {widthDimension < breakpoint ? (<AtratiLogoMobile />) : (<AtratiLogoComplete />)}
     </header>
   )
 }
